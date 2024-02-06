8a79029fa74c37a9cda9222d7fd815785e196e50
GDIVX <yarin107@gmail.com>
2024-02-07 01:10:52 +0200

    Added BindableProperty

---
+++ Assets/Scripts/Core/BindableProperty.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Assets.Scripts.Core
+{
+    public class BindableProperty<T> : IBindable<T>
+    {
+        private T _value;
+
+        public event Action<T> OnValueChanged;
+
+        public T Value
+        {
+            get => _value;
+            set
+            {
+                _value = value;
+                OnValueChanged?.Invoke(value);
+            }
+        }
+    }
+}
