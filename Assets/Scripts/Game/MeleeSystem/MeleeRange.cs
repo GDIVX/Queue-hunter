e89cacda1dff2b0bba69d5a6d1c55850dd3afb66
GDIVX <yarin107@gmail.com>
2024-04-13 18:17:37 +0300

    refactroed meele attack

--- Assets/Scripts/Game/MeleeSystem/MeleeRange.cs
+++ Assets/Scripts/Game/MeleeSystem/MeleeRange.cs
@@ -1,44 +1,50 @@
 using System.Collections.Generic;
+using Combat;
 using Game.MeleeSystem;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class MeleeRange : MonoBehaviour
 {
-    public static List<DamageReciver> EntityInRange;
-    
+    public static List<IDamageable> EntityInRange;
+    [SerializeField] private int damage;
+
+    public int Damage
+    {
+        get => damage;
+        private set => damage = value;
+    }
+
     private void Awake()
     {
-        EntityInRange = new List<DamageReciver>();
+        EntityInRange = new List<IDamageable>();
+    }
+
+    [Button]
+    public void DoDamage()
+    {
+        foreach (IDamageable target in EntityInRange)
+        {
+            target.HandleDamage(Damage);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (!other.gameObject.CompareTag("Enemy"))
-            if (other.gameObject.GetComponent<DamageReciver>() == null)
-                return;
-
-        Debug.Log("Add");
-        EntityInRange.Add(other.GetComponent<DamageReciver>());
+        if (other.gameObject.CompareTag("Enemy")) return;
+        if (other.TryGetComponent(out IDamageable target))
+        {
+            EntityInRange.Add(target);
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (!other.gameObject.CompareTag("Enemy"))
-        {
-            var damageReciver = other.gameObject.GetComponent<DamageReciver>();
-            
-            if (damageReciver != null)
-            {
-                if (!EntityInRange.Contains(damageReciver))
-                    return;
-                
-            }
-            else
-                return;
-        }
-            
+        if (other.gameObject.CompareTag("Enemy")) return;
 
-        Debug.Log("Remove");
-        EntityInRange.Remove(other.GetComponent<DamageReciver>());
+        if (other.TryGetComponent(out IDamageable target))
+        {
+            EntityInRange.Remove(target);
+        }
     }
 }
