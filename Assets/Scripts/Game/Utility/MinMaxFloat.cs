b0a4173c2f07d71e84bcb507979ddd45ad3c4f8d
GDIVX <yarin107@gmail.com>
2024-06-23 18:24:02 +0300

    Create MinMaxFloat.cs

---
+++ Assets/Scripts/Game/Utility/MinMaxFloat.cs
@@ -0,0 +1,14 @@
+namespace Game.Utility
+{
+    public struct MinMaxFloat
+    {
+        public float Min;
+        public float Max;
+
+        public MinMaxFloat(float min, float max)
+        {
+            Min = min;
+            Max = max;
+        }
+    }
+}
